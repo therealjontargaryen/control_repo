5fc0cbe82f5867b4507db8e9206df054d198eab2
therealjontargaryen <53835959+therealjontargaryen@users.noreply.github.com>
2019-08-07 15:13:55 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -9,8 +9,8 @@ class minecraft {
     ensure => directory
   }
   file {'/opt/minecraft/minecraft_server.jar':
-    ensure => file
-    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
   }
   package {'java':
     ensure => present
