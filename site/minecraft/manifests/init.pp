2b4a6eb99d6ed78fd3dba27d80a9c2c127ccbe5b
therealjontargaryen <53835959+therealjontargaryen@users.noreply.github.com>
2019-08-08 08:43:12 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,6 +2,11 @@ class minecraft {
   file {'/etc/profile.d/environment.sh':
     ensure => file,
     content => 'export https_proxy=http://www-proxy-hqdc.us.oracle.com:80;export http_proxy=http://www-proxy-hqdc.us.oracle.com:80;export HTTPS_PROXY=http://www-proxy-hqdc.us.oracle.com:80;export HTTP_PROXY=http://www-proxy-hqdc.us.oracle.com:80;export proxy=http://www-proxy-hqdc.us.oracle.com:80',    
+    mode => '0755',
+    notify => Exec['export_vars'],
+  }
+  exec { 'export_vars':
+    command => "/bin/bash -c '/etc/profile.d/environment.sh'",
   }
   file {'/opt/minecraft':
     ensure => directory
